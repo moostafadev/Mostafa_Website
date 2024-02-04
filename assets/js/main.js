9f1d3482fc4dd41acd687ef0c4ddad3e25f93889
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-04 04:24:21 +0200

    Finish Day

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 // Navbar Action
 const icon = document.querySelector(".i-container i");
 const nav = document.querySelector(".nav-container");
+const links = document.querySelectorAll(".nav-container li a");
 
 icon.addEventListener("click", navFun);
+
+links.forEach((li) => {
+  li.addEventListener("click", removeActive);
+});
 
 // Navbar Function
 function navFun() {
   nav.classList.toggle("active");
+}
+
+// Remove Active Class From Navbar
+function removeActive() {
+  nav.classList.remove("active");
 }
