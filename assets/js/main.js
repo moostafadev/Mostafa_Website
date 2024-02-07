5f2bea33f7bb3b5ce0c44ec0906586e93759cbe6
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-07 16:43:53 +0200

    Fix Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,19 @@
-// Navbar Action
+// Main Variables
 const icon = document.querySelector(".i-container i");
 const nav = document.querySelector(".nav-container");
 const links = document.querySelectorAll(".nav-container li a");
 
-icon.addEventListener("click", navFun);
-
-links.forEach((li) => {
-  li.addEventListener("click", removeActive);
-});
-
-// Navbar Function
-function navFun() {
-  nav.classList.toggle("active");
+function navFun(icon, nav) {
+  icon.addEventListener("click", () => nav.classList.toggle("active"));
 }
 
-// Remove Active Class From Navbar
-function removeActive() {
-  nav.classList.remove("active");
+function removeActive(links) {
+  links.forEach((li) => {
+    li.addEventListener("click", () => {
+      nav.classList.remove("active");
+    });
+  });
 }
 
-// Scroll Animation
-const sr = ScrollReveal({
-  origin: "top",
-  distance: "80px",
-  duration: 2000,
-  reset: true,
-});
-sr.reveal(".home-title", {});
-sr.reveal(".home-scroll", { delay: 200 });
-sr.reveal(".home-img", { origin: "right", delay: 300 });
-sr.reveal(".head-section", {});
-sr.reveal(".text", { delay: 200 });
-sr.reveal(".image", { delay: 300 });
-sr.reveal(".item", { interval: 200 });
+navFun(icon, nav);
+removeActive(links);
